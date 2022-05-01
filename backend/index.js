a3c245790c22b90ed10b903d8cf0b53f4859b56b
Ashlysss <73705358+Ashlysss@users.noreply.github.com>
2022-05-01 01:28:43 -0500

    integrated CockraochDB in node

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const { Client } = require("pg");
+
+(async () => {
+  const client = new Client(process.env.DATABASE_URL);
+
+  const statements = [
+    // CREATE the messages table
+    "CREATE TABLE IF NOT EXISTS messages (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), message STRING)",
+    // INSERT a row into the messages table
+    "INSERT INTO messages (message) VALUES ('Hello world!')",
+    // SELECT a row from the messages table
+    "SELECT message FROM messages",
+  ];
+
+  try {
+    // Connect to CockroachDB
+    await client.connect();
+    for (let n = 0; n < statements.length; n++) {
+      let result = await client.query(statements[n]);
+      if (result.rows[0]) { console.log(result.rows[0].message); }
+    }
+    await client.end();
+  } catch (err) {
+    console.log(`error connecting: ${err}`);
+  }
+
+  // Exit program
+  process.exit();
+})().catch((err) => console.log(err.stack));
+
+//Start Server
+app.listen(3000,function(){
+  console.log('Server started on port 3000.......');
+});
